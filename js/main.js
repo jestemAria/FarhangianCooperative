169738e35d903320f92004468400d0f65fd4c9f8
jestemAria <radmehr.krash@gmail.com>
2022-07-17 20:06:55 +0430

    necessary changes added to the repository

--- js/main.js
+++ js/main.js
@@ -332,48 +332,48 @@ $(function() {
 	];
 		
 	//set google map options
-	var map_options = {
-      	center: new google.maps.LatLng($latitude, $longitude),
-      	zoom: $map_zoom,
-      	panControl: false,
-      	zoomControl: false,
-      	mapTypeControl: false,
-      	streetViewControl: false,
-      	mapTypeId: google.maps.MapTypeId.ROADMAP,
-      	scrollwheel: false,
-      	styles: style,
-    }
-    //inizialize the map
-	var map = new google.maps.Map(document.getElementById('google-container'), map_options);
-	//add a custom marker to the map				
-	var marker = new google.maps.Marker({
-	  	position: new google.maps.LatLng($latitude, $longitude),
-	    map: map,
-	    visible: true,
-	 	icon: $marker_url,
-	});
+	// var map_options = {
+    //   	center: new google.maps.LatLng($latitude, $longitude),
+    //   	zoom: $map_zoom,
+    //   	panControl: false,
+    //   	zoomControl: false,
+    //   	mapTypeControl: false,
+    //   	streetViewControl: false,
+    //   	mapTypeId: google.maps.MapTypeId.ROADMAP,
+    //   	scrollwheel: false,
+    //   	styles: style,
+    // }
+    // //inizialize the map
+	// var map = new google.maps.Map(document.getElementById('google-container'), map_options);
+	// //add a custom marker to the map				
+	// var marker = new google.maps.Marker({
+	//   	position: new google.maps.LatLng($latitude, $longitude),
+	//     map: map,
+	//     visible: true,
+	//  	icon: $marker_url,
+	// });
 
-	//add custom buttons for the zoom-in/zoom-out on the map
-	function CustomZoomControl(controlDiv, map) {
-		//grap the zoom elements from the DOM and insert them in the map 
-	  	var controlUIzoomIn= document.getElementById('cd-zoom-in'),
-	  		controlUIzoomOut= document.getElementById('cd-zoom-out');
-	  	controlDiv.appendChild(controlUIzoomIn);
-	  	controlDiv.appendChild(controlUIzoomOut);
+	// //add custom buttons for the zoom-in/zoom-out on the map
+	// function CustomZoomControl(controlDiv, map) {
+	// 	//grap the zoom elements from the DOM and insert them in the map 
+	//   	var controlUIzoomIn= document.getElementById('cd-zoom-in'),
+	//   		controlUIzoomOut= document.getElementById('cd-zoom-out');
+	//   	controlDiv.appendChild(controlUIzoomIn);
+	//   	controlDiv.appendChild(controlUIzoomOut);
 
-		// Setup the click event listeners and zoom-in or out according to the clicked element
-		google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
-		    map.setZoom(map.getZoom()+1)
-		});
-		google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
-		    map.setZoom(map.getZoom()-1)
-		});
-	}
+	// 	// Setup the click event listeners and zoom-in or out according to the clicked element
+	// 	google.maps.event.addDomListener(controlUIzoomIn, 'click', function() {
+	// 	    map.setZoom(map.getZoom()+1)
+	// 	});
+	// 	google.maps.event.addDomListener(controlUIzoomOut, 'click', function() {
+	// 	    map.setZoom(map.getZoom()-1)
+	// 	});
+	// }
 
-	var zoomControlDiv = document.createElement('div');
- 	var zoomControl = new CustomZoomControl(zoomControlDiv, map);
+	// var zoomControlDiv = document.createElement('div');
+ 	// var zoomControl = new CustomZoomControl(zoomControlDiv, map);
 
-  	//insert the zoom div on the top left of the map
-  	map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
+  	// //insert the zoom div on the top left of the map
+  	// map.controls[google.maps.ControlPosition.LEFT_TOP].push(zoomControlDiv);
 
 });
